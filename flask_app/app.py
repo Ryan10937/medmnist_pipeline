7ff81b9f843731f61f705def2be7bbebc5e6a5bd
Ryan10937 <ryan@sterzenbach.com>
2025-01-21 18:59:23 -0600

    added flask app that allows for a single image upload and inference

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template,render_template_string
 import os
 import subprocess
-
+import pandas as pd
 app = Flask(__name__)
 
 # Folder to store uploaded images
@@ -34,13 +34,40 @@ def upload_file():
         result = subprocess.run(['python', 'inference.py', filename], capture_output=True, text=True)
         
         # Handle the output from the inference script (result.stdout)
-        return f'Inference result: {result.stdout}'
+        return f'Image uploaded'
+        # return f'Inference result: {result.stdout}'
     else:
         return 'Invalid file type. Only images are allowed.'
+@app.route('/infer', methods=['GET'])
+def infer():
+    result = subprocess.run(['./inference_script.sh'], check=True, capture_output=True, text=True)
+    results_df = pd.read_csv('results/predictions.csv')
+    df_html = results_df.to_html(classes='table table-bordered', index=False)
+    
+    # Render the DataFrame as an HTML page
+    return render_template_string("""
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>DataFrame Display</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        </head>
+        <body>
+            <div class="container mt-5">
+                <h1>Displaying Pandas DataFrame</h1>
+                <div class="table-responsive">
+                    {{ df_html|safe }}
+                </div>
+            </div>
+        </body>
+        </html>
+    """, df_html=df_html)
 
 if __name__ == '__main__':
     # Ensure the upload folder exists
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=9000)
+
