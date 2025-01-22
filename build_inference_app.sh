778b553175b71732554161abb6100a498ea96d5d
Ryan10937 <ryan@sterzenbach.com>
2025-01-21 23:46:53 -0600

    added port mapping to build inference shell script

--- build_inference_app.sh
+++ build_inference_app.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker build -f Dockerfile -t medmnist_inference .
-# docker run -v ./results:/app/results -e MODEL_PATH=models/model.keras -e DATA_FOLDER=uploads -e IMAGE_SIZE=64 medmnist_inference
-docker run -v ./results:/app/results medmnist_inference
+docker run -v ./results:/app/results -p 9000:9000 medmnist_inference 
